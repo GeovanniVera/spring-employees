818426c7011cf671e8bf6a39cb9494a7cdb57a0a
geovanni vera <geovanni.vera23@gmail.com>
2025-05-09 11:54:37 -0600

    primer comit

---
+++ src/main/java/com/employees/employeesapp/EmployeesappApplication.java
@@ -0,0 +1,13 @@
+package com.employees.employeesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesappApplication.class, args);
+	}
+
+}
