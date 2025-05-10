57a76a1aafe57b2d948c74eb0f4f505a96e066e9
Geovanni-Vera <geovanni.vera23@gmail.com>
2025-05-10 16:32:21 -0600

    se agrego el layout principal

--- src/main/java/com/employees/employeesapp/controller/HomeController.java
+++ src/main/java/com/employees/employeesapp/controller/HomeController.java
@@ -1,17 +1,27 @@
 package com.employees.employeesapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String index() {
-        return new String("index");
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("pageTitle", "Inicio");
+        return "index"; // O "home" si usas home.html
     }
-    
 
-    
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("pageTitle", "Inicio");
+        return "index"; // O "home"
+    }
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("pageTitle", "Test");
+        return "test"; // O "home"
+    }
 }
